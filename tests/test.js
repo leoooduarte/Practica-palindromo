3f27a99e1c4875b23c297da0bf83f58c82e7db03
leoooduarte <leitohcarp@gmail.com>
2019-08-21 19:39:54 -0300

    test

--- tests/test.js
+++ tests/test.js
@@ -2,10 +2,34 @@ var expect = chai.expect;
 
 describe("Probando la funcion Palindromo", function(){
 
- it("Comparación", function(){
+ it("Comparación de palabra ama", function(){
   
   expect(palindromo("ama")).to.be.equal(true);
   
  })
-
+ it("Comparación de palabra narran", function(){
+  
+  expect(palindromo("narran")).to.be.equal(true);
+  
+ })
+ it("Comparación de palabra papa", function(){
+  
+  expect(palindromo("papa")).to.be.equal(true);
+  
+ })
+ it("Comparación de palabra ama ama", function(){
+  
+  expect(palindromo("ama ama")).to.be.equal(true);
+  
+ })
+ it("Comparación de palabra papa ama", function(){
+  
+  expect(palindromo("papa ama")).to.be.equal(true);
+  
+ })
+ it("Comparación de string vacio", function(){
+  
+  expect(palindromo("")).to.be.equal(true);
+  
+ })
 })
